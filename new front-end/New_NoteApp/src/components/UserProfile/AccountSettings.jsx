762cdd1a9858b2e0b992ac597d4e798a34414398
Bruno Janeiro <janeiro.bruno23@gmail.com>
2024-01-20 10:10:05 +0100

    Edit profile done with phone number country

--- new front-end/New_NoteApp/src/components/UserProfile/AccountSettings.jsx
+++ new front-end/New_NoteApp/src/components/UserProfile/AccountSettings.jsx
@@ -1,61 +1,81 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
+import AvatarEditor from 'react-avatar-editor'
 import '../../assets/css/accountSettings.css'
+import avatar from "../../images/man_avatar.png"
+import upload from "../../images/cloud-computing.png"
+import PhoneNumberValidation from './PhoneNumberValidation'
+
 
 export function AccountSettings() {
+
+    const inputRef = useRef(null)
+    
+    const handleImageClick = () => {
+      inputRef.current.click()
+    }
+
   return (
     <section className='accountsettings'>
-      <h1 className='mainhead1'>Your Details</h1>
+      <h1 className='text'>My Details</h1>
 
-      
+
+
         <form action= "" method="POST" className="form">
           <div>
+
+          <div className="form-group" style={{display:"flex", justifyContent:"center", marginTop:"none"}}>
+            <img className="profile_edit" style={{cursor: "pointer"}} onClick={handleImageClick}  src={avatar} alt="" />
+            <input className="" style={{display:"none"}} type="file" ref={inputRef}/>
+           </div>
+
+            
+
+
             <div className="form-group">
               <input className="form-control" type="text" name="first_name" id="first_name" placeholder="first_name"/>
-              <label htmlFor="first_name">First Name</label>
+              <label htmlFor="first_name" className='form_label'>First Name</label>
             </div>
 
             <div className="form-group">
               <input className="form-control" type="text" name="last_name" id="last_name" placeholder="last_name"/>
-              <label htmlFor="last_name">Last Name</label>
+              <label htmlFor="last_name" className='form_label'>Last Name</label>
             </div>
 
             <div className="form-group">
               <input className="form-control" type="text" name="address" id="address" placeholder="address"/>
-              <label htmlFor="address">Address</label>
+              <label htmlFor="address" className='form_label'>Address</label>
             </div>
-
-            <div className="form-group">
-              <input className="form-control" type="text" name="mobile" id="mobile" placeholder="mobile"/>
-              <label htmlFor="mobile">Mobile</label>
-            </div>
-
+            <PhoneNumberValidation/>
 
           </div>
+
+          
           
           <div className="column">
+            
             <div className="form-group">
               <input className="form-control" type="text" name="website" id="website" placeholder="website"/>
-              <label htmlFor="website">Website</label>
+              <label htmlFor="website" className='form_label'>Website</label>
             </div>
 
             <div className="form-group">
               <input className="form-control" type="text" name="facebook" id="facebook" placeholder="facebook"/>
-              <label htmlFor="facebook">Facebook</label>
+              <label htmlFor="facebook" className='form_label'>Facebook</label>
             </div>
             
             <div className="form-group">
               <input className="form-control" type="text" name="git_hub" id="git_hub" placeholder="git_hub"/>
-              <label htmlFor="git_hub">GitHub</label>
+              <label htmlFor="git_hub" className='form_label'>GitHub</label>
             </div>
 
             <div className="form-group">
               <input className="form-control" type="text" name="linkedin" id="linkedin" placeholder="linkedin"/>
-              <label htmlFor="linkedin">LinkedIn</label>
+              <label htmlFor="linkedin" className='form_label'>LinkedIn</label>
             </div>
           </div>
         </form>
       
-      <button className='submit'>
+      <button className='submit_edit'>
         Save Changes
       </button>
     </section>
---
+++ new front-end/New_NoteApp/src/components/UserProfile/PhoneNumberValidation.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import PhoneInput from 'react-phone-input-2';
+import 'react-phone-input-2/lib/style.css'
+
+
+
+function PhoneNumberValidation() {
+
+    const [PhoneNumber, setPhoneNumber] = useState('')
+    const [valid, setValid] = useState(true)
+
+    const handleChange = (value) => {
+        setPhoneNumber(value);
+        setValid(validatePhoneNumber(value))
+    };
+
+    const validatePhoneNumber = (phoneNumber) => {
+        const PhoneNumberPattern = /^\d{12}$/
+        return PhoneNumberPattern.test(phoneNumber)
+    }
+
+
+  return (
+    <div className="form-group">
+              <PhoneInput 
+              country={'pt'}
+              international
+              containerStyle={{
+                border:""
+              }}
+              className="input-phone-number" 
+              inputStyle={{
+                borderStyle: "none none solid none",
+                borderColor: "gray",
+                width: "100%",
+                color: "goldenrod"
+              }}
+              flagButtonStyle={{color: "white"}}
+              inputProps={{required : true}}
+              value={PhoneNumber} 
+              onChange={handleChange} 
+              placeholder="mobile" />
+              {!valid && <p style={{fontSize: "8px", color: "red", marginTop: "none"}}>Please enter a valid 9-digit phone number </p>}
+    </div>
+  )
+}
+
+export default PhoneNumberValidation
