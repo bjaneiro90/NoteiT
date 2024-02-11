82166c783ee1c4e9a85dfa6f8ff9ab946cb8c8fa
Bruno Janeiro <janeiro.bruno23@gmail.com>
2024-02-11 12:51:57 +0100

    New note card with categories & public / private

--- new front-end/New_NoteApp/src/components/NoteCard.jsx
+++ new front-end/New_NoteApp/src/components/NoteCard.jsx
@@ -9,13 +9,38 @@ export const NoteCard = () => {
     return (
         <div className='note-card'>
             <form action="">
-            <div className="form-group-notecard">
-              <input className="form-control-notecard" type="text" name="first_name" id="first_name" placeholder="Titulo"/>
-            </div>
-            <div className="form-group-notecard-texteditor">
-              <TextEditor className='text-editor'  /> 
-            </div>
-              {/* <Jodit/> */}
+              <div className="form-group-notecard">
+               <input className="form-control-notecard" type="text" name="first_name" id="first_name" placeholder="Titulo"/>
+              </div>
+              <div className="form-group-notecard-texteditor">
+                 <TextEditor   /> 
+              </div>
+              <div className="form-group-notecard-category">
+                <select className="category-selection" id="number" name="category_id">
+                <option className='choose' selected disabled >Choose a Category</option>
+                <option value="1">Tecnologia</option>
+                <option value="2">Actuality</option>
+                <option value="3">Gastronomy</option>
+                <option value="4">Travels</option>
+                <option value="5">Animals</option>
+                <option value="6">Music</option>
+                <option value="7">Cinema</option>
+                <option value="8">International</option>
+                <option value="9">Culture</option>
+                <option value="10">Reminders</option>
+                </select>
+              </div>
+              <div className="form-group-notecard-public_private">
+                <div>
+                  <input type="radio" id="html" name="fav_language" value="HTML"/>
+                  <label for="html">Public</label>
+                </div>
+                <div>
+                  <input type="radio" id="html" name="fav_language" value="HTML"/>
+                  <label for="html">Private</label>
+                </div>
+              </div>
+            
             </form>
         </div>
     )
